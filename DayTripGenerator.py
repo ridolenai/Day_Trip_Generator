4609f98853f820a1b2634aceacf7304d7085fe97
ridolenai <92106359+ridolenai@users.noreply.github.com>
2021-10-07 11:18:01 -0500

    Initial Commit

---
+++ DayTripGenerator.py
@@ -0,0 +1,71 @@
+import random
+
+
+destinations = ["Disney World", "Oktoberfest", "Crawfish Festival", "Mawmaw's House"]
+restaurants = ["Applebee's", "Schnitzel Emporium", "Pizza Hut", "Sullivan's"]
+transportation = ['Car', 'Bicycle', '4-wheeler', 'Yee Yee Truck']
+entertainment = ['Miley Cyrus Concert', 'Chainsaw Juggler', 'Sock Manufacturing Tour', 'Mud-riding']
+day_trip = []
+satisfaction = ''
+
+
+def indecisive_trip (x):
+    random_trip = random.choice(x)
+    print (random_trip)
+    return (random_trip)
+
+def begin (p):
+    print("Welcome to the RNGeezus Trip Generator. This will help you find something to do for the day since you have no ideas.")
+    print ("Below, you will see your selections for the day.  If you do not like any of these suggestions, you may have them produced again.")
+    day_trip.append (indecisive_trip(destinations))
+    day_trip.append (indecisive_trip(restaurants))
+    day_trip.append (indecisive_trip(transportation))
+    day_trip.append (indecisive_trip(entertainment))
+
+
+def satisfaction (h):
+    satisfaction = input ('Please type y if you are satisfied or n if you are not')
+    return (satisfaction)
+
+def choose_again (q):
+     if satisfaction == ('y'):
+         print('We are glad you are satisfied with your selection. Please have a pleasant trip and remember to wear your seatbelt.')
+     if satisfaction == ('n'):
+         unsatisfied = (input('Please select the option you would like to change: 1:destination, 2:restaurant, 3:transportation, or 4:entertainment '))
+
+         if unsatisfied == ('1'):
+             day_trip.pop(0)
+             day_trip.insert(0, indecisive_trip(destinations))
+            
+         elif unsatisfied == ('2'):
+             day_trip.pop(1)
+             day_trip.insert(1, indecisive_trip(restaurants))
+             satisfied = input ("Please type y if you are satisfied or n for not satisfied. ")
+            
+         elif unsatisfied == ('3'):
+             day_trip.pop(2)
+             day_trip.insert(2, indecisive_trip(transportation))
+             satisfied = input ("Please type y if you are satisfied or n for not satisfied. ")
+            
+         elif unsatisfied == ('4'):
+             day_trip.pop(3)
+             day_trip.insert(3, indecisive_trip(entertainment))
+             satisfied = input ("Please type y if you are satisfied or n for not satisfied. ")
+     return day_trip    
+
+begin(indecisive_trip)
+
+while satisfaction != ('y'):
+    #begin(indecisive_trip)
+    choose_again(indecisive_trip)
+    satisfaction = input ('Please type y if you are satisfied or n if you are not')
+    #if satisfaction == ('y'):
+print ('Here is your randomly generated day trip:')
+print (day_trip)
+print ("Thank you for using the RNGeezus Trip Generator.  Enjoy your day away! ")
+
+
+
+
+
+
