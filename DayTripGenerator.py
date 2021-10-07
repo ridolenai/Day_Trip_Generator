27d07e2a977c8f5682d57d86713eea165597bb27
ridolenai <92106359+ridolenai@users.noreply.github.com>
2021-10-07 12:03:56 -0500

    Explanation
    Added comments to describe functions.

--- DayTripGenerator.py
+++ DayTripGenerator.py
@@ -9,12 +9,12 @@ day_trip = []
 satisfaction = ''
 
 
-def indecisive_trip (x):
+def indecisive_trip (x):  #Selects random options from lists.
     random_trip = random.choice(x)
     print (random_trip)
     return (random_trip)
 
-def begin (p):
+def begin (p):#Keeps track of choices in list.
     print("Welcome to the RNGeezus Trip Generator. This will help you find something to do for the day since you have no ideas.")
     print ("Below, you will see your selections for the day.  If you do not like any of these suggestions, you may have them produced again.")
     day_trip.append (indecisive_trip(destinations))
@@ -22,12 +22,7 @@ def begin (p):
     day_trip.append (indecisive_trip(transportation))
     day_trip.append (indecisive_trip(entertainment))
 
-
-def satisfaction (h):
-    satisfaction = input ('Please type y if you are satisfied or n if you are not')
-    return (satisfaction)
-
-def choose_again (q):
+def choose_again (q): #Enables user to select another option if one of the selections is undesirable.
      if satisfaction == ('y'):
          print('We are glad you are satisfied with your selection. Please have a pleasant trip and remember to wear your seatbelt.')
      if satisfaction == ('n'):
@@ -55,12 +50,13 @@ def choose_again (q):
 
 begin(indecisive_trip)
 
-while satisfaction != ('y'):
+while satisfaction != ('y'): #allows users to change undesirable options until they are satisfied.
     #begin(indecisive_trip)
     choose_again(indecisive_trip)
     satisfaction = input ('Please type y if you are satisfied or n if you are not')
     #if satisfaction == ('y'):
-print ('Here is your randomly generated day trip:')
+
+print ('Here is your randomly generated day trip:')#These three lines print the outcome once the user is satisfied.
 print (day_trip)
 print ("Thank you for using the RNGeezus Trip Generator.  Enjoy your day away! ")
 
